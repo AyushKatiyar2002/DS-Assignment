acce13f9e4a051baf97ebdd59521dbbc9d2aaedb
AyushKatiyar2002 <55269230+AyushKatiyar2002@users.noreply.github.com>
2020-04-20 20:20:47 +0530

    Add files via upload

---
+++ BST and AVL.c
@@ -0,0 +1,35 @@
+/*
+1. Construct a BST with following data –
+   1,2,3,4,5,6
+   And find type of imbalancing if any, an unbalance node is one in which the balance factor is
+   not among {-1, 0, 1}. Consider Leaf node height as 1.
+*/
+struct Node* insert(struct Node* node, int key)
+{
+    if (node == NULL)
+        return(newNode(key));
+
+    if (key < node->key)
+        node->left  = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else
+        return node;
+    node->height = 1 + max(height(node->left),height(node->right));
+    int balance = getBalance(node);
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+    if (balance > 1 && key > node->left->key)
+    {
+        node->left =  leftRotate(node->left);
+        return rightRotate(node);
+    }
+    if (balance < -1 && key < node->right->key)
+    {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+    return node;
+}
